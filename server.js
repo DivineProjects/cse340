0280eaad57080c8ee0a26a3dc8a1b04ab7e7a1df
divineProjects <divinejigu@gmail.com>
2024-04-26 16:59:15 +0200

    Build the index.ejs View and updateed server.js

--- server.js
+++ server.js
@@ -22,6 +22,9 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
