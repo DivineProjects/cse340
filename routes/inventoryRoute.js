46a6fe2cb05f9945faaff7c251362af9076b8cf4
divineProjects <divinejigu@gmail.com>
2024-05-09 14:11:03 +0200

    modified the index.js, inventoryRoute.js, inventory-models and invController

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -4,4 +4,4 @@ const router = new express.Router();
 const invController = require("../controllers/invController");
 
 // Route to build inventory by classification view
-router.get("/type/:classifcationId", invController.bulidByClassificationId)
+router.get("/type/:classifcationId", invController.buildByClassificationId);
