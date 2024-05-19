9940fe790fa1fb5518bdffbc4bc0a4f4363743e0
divineProjects <divinejigu@gmail.com>
2024-05-19 03:27:49 +0200

    final week3

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -2,7 +2,7 @@ const utilities = require("../utilities/")
 const errorController = {}
 
 errorController.builError = async function(req, res){
-  // const nav = await utilities.getNav()
+  const nav = await utilities.getNav()
   res.render("index", {title: "Error 505", nav})
 }
 
