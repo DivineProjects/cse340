17557faead565eb4a6f5e48005a86078e674dba2
divineProjects <divinejigu@gmail.com>
2024-05-26 01:17:59 +0200

    Registration Route server side validation in accountsRoute.js file

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,13 +3,17 @@ const express = require("express")
 const router = new express.Router() 
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities/")
+const regValidation = require("../utilities/account-validation")
 
 
 // Route to build Login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 // Route to build registration view
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
-router.post("/register", utilities.handleErrors(accountController.registerAccount));
+router.post("/register", 
+    regValidation.registationRules(),
+    regValidation.checkRegData, 
+    utilities.handleErrors(accountController.registerAccount));
 
 
 module.exports = router;
