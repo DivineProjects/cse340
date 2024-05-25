82dbb423ff901c80f0790bd77197e552f330cb6a
divineProjects <divinejigu@gmail.com>
2024-05-25 15:48:35 +0200

    account register form page added

---
+++ models/account-model.js
@@ -0,0 +1,9 @@
+const pool = require("../database/")
+
+/* *****************************
+*   Register new account
+* *************************** */
+
+async function registerAccount(account_firstname, account_lastname, account_email, account_password) {
+    
+}
